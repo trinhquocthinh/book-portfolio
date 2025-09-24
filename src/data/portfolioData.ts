a5a83732e9863bed8694ed917ea345b420e21a2e
thinh <thinh.trinh@izion24.com.vn>
2025-09-24 11:32:35 +0700

    fix url image

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -1,26 +1,27 @@
-import type { 
-  PersonalInfo, 
-  SocialMedia, 
-  WorkExperienceItem, 
-  EducationItem, 
-  ServiceItem, 
-  SkillCategory, 
-  Project 
-} from '../types';
+import type {
+  PersonalInfo,
+  SocialMedia,
+  WorkExperienceItem,
+  EducationItem,
+  ServiceItem,
+  SkillCategory,
+  Project,
+} from "../types";
 
 export const portfolioData = {
   personalInfo: {
     name: "Mr Skeleton",
     title: "Web Developer",
-    description: "Hi, I'm a web Developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque commodi numquam dolor a deserunt incidunt esse molestias voluptatum porro ullam.",
-    profileImage: "/Logo.png"
+    description:
+      "Hi, I'm a web Developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque commodi numquam dolor a deserunt incidunt esse molestias voluptatum porro ullam.",
+    profileImage: "./Logo.png",
   } as PersonalInfo,
 
   socialMedia: {
     facebook: "#",
     twitter: "#",
     instagram: "#",
-    linkedin: "#"
+    linkedin: "#",
   } as SocialMedia,
 
   workExperience: [
@@ -29,22 +30,25 @@ export const portfolioData = {
       period: "2018 - 2020",
       position: "Web Developer",
       company: "Software Pro",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?"
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?",
     },
     {
       id: "work2",
       period: "2021 - 2022",
       position: "Web Designer",
       company: "Software Pro",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?"
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?",
     },
     {
       id: "work3",
       period: "2022 - 2023",
       position: "Graphic Designer",
       company: "Software Pro",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?"
-    }
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?",
+    },
   ] as WorkExperienceItem[],
 
   education: [
@@ -53,22 +57,25 @@ export const portfolioData = {
       period: "2016 - 2018",
       degree: "College",
       institution: "Local College",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?"
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?",
     },
     {
       id: "edu2",
       period: "2018 - 2020",
       degree: "Intermediate",
       institution: "University",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?"
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?",
     },
     {
       id: "edu3",
       period: "2020 - 2024",
       degree: "BS Computer Science",
       institution: "University",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?"
-    }
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae minus eius nemo. Optio cupiditate nostrum commodi repellendus nemo quod in?",
+    },
   ] as EducationItem[],
 
   services: [
@@ -76,26 +83,26 @@ export const portfolioData = {
       id: "service1",
       icon: "bx bx-code-alt",
       title: "Web Development",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam."
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam.",
     },
     {
       id: "service2",
       icon: "bx bxs-paint",
       title: "Creative Design",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam."
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam.",
     },
     {
       id: "service3",
       icon: "bx bx-bar-chart-alt",
       title: "Digital Marketing",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam."
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam.",
     },
     {
       id: "service4",
       icon: "bx bx-search",
       title: "SEO",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam."
-    }
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quae accusamus aliquam.",
+    },
   ] as ServiceItem[],
 
   skills: [
@@ -108,8 +115,8 @@ export const portfolioData = {
         { name: "Angular", icon: "bx bxl-angular" },
         { name: "React Js", icon: "bx bxl-react" },
         { name: "BootStrap", icon: "bx bxl-bootstrap" },
-        { name: "Jquery", icon: "bx bxl-jquery" }
-      ]
+        { name: "Jquery", icon: "bx bxl-jquery" },
+      ],
     },
     {
       category: "Back-End",
@@ -117,24 +124,23 @@ export const portfolioData = {
         { name: "Python", icon: "bx bxl-python" },
         { name: "Java", icon: "bx bxl-java" },
         { name: "Node JS", icon: "bx bxl-nodejs" },
-        { name: "PHP", icon: "bx bxl-php" }
-      ]
+        { name: "PHP", icon: "bx bxl-php" },
+      ],
     },
     {
       category: "UI/UX Design",
-      skills: [
-        { name: "Figma", icon: "bx bxl-figma" }
-      ]
-    }
+      skills: [{ name: "Figma", icon: "bx bxl-figma" }],
+    },
   ] as SkillCategory[],
 
   latestProject: {
     id: "project1",
     title: "Project Name",
-    image: "/website 3.png",
+    image: "./website 3.png",
     techUsed: "React, TypeScript, SCSS",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, vel ipsum quis possimus quaerat voluptatem temporibus saepe veniam?",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, vel ipsum quis possimus quaerat voluptatem temporibus saepe veniam?",
     liveUrl: "#",
-    sourceUrl: "#"
-  } as Project
+    sourceUrl: "#",
+  } as Project,
 };
